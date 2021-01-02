49a7f0625160bdb5c1e4d0f5b35ab00e45b1276b
Vincent Horvath <vhorvath2010@gmail.com>
2021-01-02 09:19:26 -0500

    Initial commit

---
+++ src/main/java/com/vhbob/airidaleinvutils/AiridaleInvUtils.java
@@ -0,0 +1,11 @@
+package com.vhbob.airidaleinvutils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class AiridaleInvUtils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+}
