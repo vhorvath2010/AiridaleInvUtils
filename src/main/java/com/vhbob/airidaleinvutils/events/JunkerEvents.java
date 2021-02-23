fef0997c1d573cd724a697b924f24572073355b0
Vince <vhorvath2010@gmail.com>
2021-02-22 18:15:53 -0800

    Rounding

--- src/main/java/com/vhbob/airidaleinvutils/events/JunkerEvents.java
+++ src/main/java/com/vhbob/airidaleinvutils/events/JunkerEvents.java
@@ -22,12 +22,21 @@ public class JunkerEvents implements Listener {
             }
             // Give money
             if (total > 0) {
-                AiridaleInvUtils.getEconomy().depositPlayer((OfflinePlayer) event.getPlayer(), total);
-                event.getPlayer().sendMessage(ChatColor.GREEN + "+$" + total);
+                AiridaleInvUtils.getEconomy().depositPlayer((OfflinePlayer) event.getPlayer(), total * 100 / 100);
+                event.getPlayer().sendMessage(ChatColor.GREEN + "+$" + round(total, 2));
             } else {
                 event.getPlayer().sendMessage(ChatColor.RED + "You did not junk anything valuable");
             }
         }
+    }
+
+    public static double round(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+
+        long factor = (long) Math.pow(10, places);
+        value = value * factor;
+        long tmp = Math.round(value);
+        return (double) tmp / factor;
     }
 
 }
